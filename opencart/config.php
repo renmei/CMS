254b637b3dbaf5480c5461e038502598f90da1eb
renmei <avaya272@gmail.com>
2015-05-06 13:48:03 +0200

    Dodanie opencart

---
+++ opencart/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://zetrov.pl/extranet/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://zetrov.pl/extranet/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/www/zetrov.pl/extranet/opencart/catalog/');
+define('DIR_SYSTEM', '/home/www/zetrov.pl/extranet/opencart/system/');
+define('DIR_LANGUAGE', '/home/www/zetrov.pl/extranet/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/home/www/zetrov.pl/extranet/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/home/www/zetrov.pl/extranet/opencart/system/config/');
+define('DIR_IMAGE', '/home/www/zetrov.pl/extranet/opencart/image/');
+define('DIR_CACHE', '/home/www/zetrov.pl/extranet/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/home/www/zetrov.pl/extranet/opencart/system/download/');
+define('DIR_UPLOAD', '/home/www/zetrov.pl/extranet/opencart/system/upload/');
+define('DIR_MODIFICATION', '/home/www/zetrov.pl/extranet/opencart/system/modification/');
+define('DIR_LOGS', '/home/www/zetrov.pl/extranet/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'SQ5MycMAZXqdAPn3');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
